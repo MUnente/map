dc57e3b1b84d8f59d88cca5232111240aeb50ef6
Murilo Manente <manentemurilo@gmail.com>
2020-07-14 13:05:54 -0300

    new commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,10 +8,15 @@ import './App.css';
 export default function App() {
   const [inputAddress, setInputAddress] = useState({ address: '' });
 
+  const [resultListAddress, setResultListAddress] = useState([]);
+  const [resultListOperator, setResultListOperator] = useState([false]);
+  
+  // console.log(resultListAddress);
+
   const [userPosition, setUserPosition] = useState([0, 0]);
   const [decimalDegree, setDecimalDegree] = useState([0, 0]);
-
-  const [operator, setOperator] = useState([false]);
+  
+  const [markerOperator, setMarkerOperator] = useState([false]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -21,15 +26,31 @@ export default function App() {
       setDecimalDegree([latitude, longitude]);
     });
   }, []);
-
+  
   function handleInputChange(event) {
     const {value} = event.target;
     
     setInputAddress({address: value});
   }
+
+  function handleMarkerOnMap(id) {
+    const {lat, lng} = id;
+    setDecimalDegree([lat, lng]);
+    setMarkerOperator([true]);
+  }
+  
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      searchAddress();
+    }
+  }
+
+  function clearFields() {
+    setResultListOperator([false]);
+  }
   
   function searchAddress() {
-    if (inputAddress.address === '' ) {
+    if (inputAddress.address === '') {
       alert('Por favor, digite um endereço');
     }
     else {
@@ -38,20 +59,41 @@ export default function App() {
       api.get(`geocode`, {
         params: {
           q: uri,
-          apiKey: 'YOUR_API_KEY',
+          apiKey: '',
         }
       }).then(response => {
         if (response.data.items.length === 0) {
           alert('Foi encontrado um total de 0 resultados para sua busca');
         }
         else {
-          const {lat, lng} = response.data.items[0].position;
-          setDecimalDegree([lat, lng]);
-          setOperator([true]);
+          setResultListAddress(response.data.items);
+          setResultListOperator([true]);
         }
       }).catch(() => {
-        alert('Ocorreu um erro ao conectar a API ao servidor');
+        alert('Ocorreu um erro ao conectar a API ao servidor. Tente mais tarde');
       });
+    }
+  }
+   
+  function Result(props) {
+    const changed = props.isChanged;
+    if (changed) {
+      return (
+        <div className="result-box">
+          {resultListAddress.map(result => (
+            <div key={result.id} className="item-address" onClick={() => handleMarkerOnMap(result.position)}>
+              <span>
+                <i className="fas fa-map-marker-alt"></i>
+              </span>
+              <div>{result.address.label}</div>
+            </div>
+          ))}
+          <div className="btn-clear-fields" onClick={() => clearFields()}>Limpar resultados</div>
+        </div>
+      );
+    }
+    else {
+      return null;
     }
   }
 
@@ -64,14 +106,18 @@ export default function App() {
       return null;
     }
   }
-
+  
   return (
     <>
-      <div className="search-box">
-        <input type="text" onChange={handleInputChange} placeholder="Digite um endereço" />
-        <span onClick={searchAddress}>
-          <i className="fas fa-search"></i>
-        </span>
+      <div id="field">
+        <div className="search-box">
+          <input type="text" onChange={handleInputChange} placeholder="Digite um endereço" onKeyPress={handleKeyPress} />
+          <span onClick={searchAddress}>
+            <i className="fas fa-search"></i>
+          </span>
+        </div>
+        
+        <Result isChanged={resultListOperator[0]} />
       </div>
       
       <Map center={decimalDegree} zoom={15} zoomControl={false}>
@@ -84,7 +130,7 @@ export default function App() {
         <CircleMarker center={userPosition} color="blue" radius={2}>
           <Popup>Você está aqui</Popup>
         </CircleMarker>
-        <Point isChanged={operator[0]} />
+        <Point isChanged={markerOperator[0]} />
       </Map>
     </>
   );
